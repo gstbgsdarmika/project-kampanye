9ceec5f6474778aeac2716c6de14f01065b2e81f
kartika28 <kartikanovi28@gmail.com>
2023-10-31 19:21:35 +0800

    update responsif in program section

--- src/components/ProgramCard.jsx
+++ src/components/ProgramCard.jsx
@@ -2,12 +2,16 @@ export default function ProgramCard({ title, subtitle, imageUrl, link }) {
   return (
     <div className="items-center justify-center p-4 mx-3 bg-white border shadow-md md:mx-0 md:rounded-3xl rounded-2xl">
       <div className="md:rounded-3xl rounded-2xl">
-        <img src={imageUrl} alt="Card Image" />
+        <img
+          className="object-cover w-full rounded-2xl"
+          src={imageUrl}
+          alt="Card Image"
+        />
       </div>
-      <p className="pt-4 font-bold text-center text-black md:text-2xl">
-        {title}
+      <p className="pt-4 font-bold text-center text-black text-2xl">{title}</p>
+      <p className="text-[#676363] text-center text-base md:text-lg pt-2 mb-4 pb-8 overflow-ellipsis overflow-hidden line-clamp-3 md:max-h-24 max-h-20">
+        {subtitle}
       </p>
-      <p className="text-[#676363] text-center text-lg pt-2 pb-4">{subtitle}</p>
       <a
         href={link}
         target="_blank"
